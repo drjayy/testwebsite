f4ad6b0793aa00a9f692060f78566848fac27d2d
jayyythewaffle <jaayy.private@gmail.com>
2022-06-27 23:45:54 +0200

    Update Page

--- src/Page.js
+++ src/Page.js
@@ -1,10 +1,13 @@
-import './App.css';
+import './Page.css';
 
 function Page() {
   return (
     <div className="Page">
       <header className="Page-header">
       </header>
+      <footer className="Page-footer">
+        <small>© Copyright 2022 DrJayy. All rights reserved.</small>
+      </footer>
     </div>
   );
 }
