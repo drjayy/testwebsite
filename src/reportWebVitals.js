77f662f69457018bd53cc3682712022210b93614
jayyythewaffle <jaayy.private@gmail.com>
2022-06-28 00:01:01 +0200

    Add Documentation

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,3 +1,7 @@
+/**
+ * It imports the web-vitals package and then calls the getCLS, getFID, getFCP, getLCP, and getTTFB
+ * functions from that package, passing in the onPerfEntry function as an argument
+ */
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
