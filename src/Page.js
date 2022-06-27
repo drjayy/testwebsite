1b2c35368d1545b2739f3cad187a307682074dc3
jayyythewaffle <jaayy.private@gmail.com>
2022-06-27 23:41:30 +0200

    Rename App to Page

---
+++ src/Page.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function Page() {
+  return (
+    <div className="Page">
+      <header className="Page-header">
+      </header>
+    </div>
+  );
+}
+
+export default Page;
